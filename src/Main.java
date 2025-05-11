c73b8d2545c20a1f2ef7e72f886ad77ca809c427
taivs93 <taivs93@gmail.com>
2025-05-11 16:59:42 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,143 @@
+import java.util.*;
+
+public class Main {
+    static List<String> lastFiveCalculations = new LinkedList<>();
+    static Double lastResult;
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int choice;
+        while(true){
+            menu();
+            choice = scanner.nextInt();
+            if(choice == 1){
+                System.out.print("Nhập a: ");
+                double a = input();
+                System.out.print("Nhập b: ");
+                double b = input();
+                calculationForm();
+                choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        sum(a,b);
+                        break;
+                    case 2:
+                        minus(a,b);
+                        break;
+                    case 3:
+                        multiply(a,b);
+                        break;
+                    case 4:
+                        divide(a,b);
+                        break;
+                    case 5:
+                        module(a,b);
+                        break;
+                    case 6:
+                        pow(a,b);
+                        break;
+                    case 7:
+                        sqrt(a,b);
+                        break;
+
+                }
+            }
+            else if (choice == 2) {
+                if(lastFiveCalculations.isEmpty()) System.out.println("Bạn chưa thực hiện phép tính nào");
+                else lastFiveCalculations.forEach(System.out::println);
+            }
+            else {
+                break;
+            }
+        }
+    }
+    private static void menu(){
+        System.out.println("Menu ");
+        System.out.println("1. Thực hiện phép tính");
+        System.out.println("2. Xem 5 phép toán gần nhất");
+        System.out.println("3. Thoát");
+        System.out.print("Lựa chọn: ");
+    }
+    private static void calculationForm(){
+        System.out.println(" Chọn phép toán ");
+        System.out.println("1. Tổng");
+        System.out.println("2. Hiệu");
+        System.out.println("3. Tích");
+        System.out.println("4. Thương");
+        System.out.println("5. Lấy dư");
+        System.out.println("6. Lũy thừa");
+        System.out.println("7. Căn bậc 2");
+        System.out.print("Lựa chọn: ");
+    }
+    private static double input(){
+        double x;
+        System.out.println("Sử dụng kết quả phép toán trước? (y/n)");
+        String choice = scanner.next();
+        if (choice.equals("y")) {
+            if(lastResult != null) {
+                x = lastResult;
+                System.out.println(x);
+            }
+            else {
+                System.out.println("Bạn chưa thực hiện phép tính nào");
+                System.out.print("Nhập lại: ");
+                x = scanner.nextDouble();
+            }
+        }
+        else{
+            System.out.print("Nhập: ");
+            x = scanner.nextDouble();
+        }
+        return x;
+    }
+    private static void addToLastFiveCalculationList(String s){
+        if (lastFiveCalculations.size() == 5) lastFiveCalculations.remove(0);
+        lastFiveCalculations.add(s);
+    }
+    private static void sum(double a, double b){
+        lastResult = a+b;
+        addToLastFiveCalculationList(String.format("%.2f + %.2f = %.2f", a, b, a + b));
+        System.out.println(lastResult);
+    }
+    private static void minus(double a,double b){
+        lastResult = a-b;
+        addToLastFiveCalculationList(String.format("%.2f - %.2f = %.2f", a, b, a - b));
+        System.out.println(lastResult);
+    }
+    private static void divide(double a,double b){
+        if(Double.compare(b,0.0 ) == 0) {
+            System.out.println("Không thể thực hiện phép chia cho 0");
+            return;
+        }
+        lastResult = a/b;
+        addToLastFiveCalculationList(String.format("%.2f / %.2f = %.2f", a, b, a / b));
+        System.out.println(lastResult);
+    }
+    private static void multiply(double a, double b){
+        lastResult = a*b;
+        addToLastFiveCalculationList(String.format("%.2f * %.2f = %.2f", a, b, a * b));
+        System.out.println(lastResult);
+    }
+    private static void module(double a, double b){
+        if (a != (int) a || b != (int) b) {
+            System.out.println("Không thể lấy dư vì a,b không phải số nguyên");
+            return;
+        }
+        lastResult = a%b;
+        addToLastFiveCalculationList(String.format("%d %% %d = %d", (int)a, (int)b, (int)a % (int)b));
+        System.out.println(lastResult);
+    }
+    private static void pow(double a, double b){
+        lastResult = Math.pow(a,b);
+        addToLastFiveCalculationList(String.format("%.2f ^ %.2f = %.2f", a, b, Math.pow(a,b)));
+        System.out.println(lastResult);
+    }
+    private static void sqrt(double a,double b){
+        System.out.printf("Căn bậc 2 của %.2f : %.2f",a,Math.sqrt(a));
+        System.out.println();
+        System.out.printf("Căn bậc 2 của %.2f : %.2f",b,Math.sqrt(b));
+        System.out.println();
+        lastResult = Math.sqrt(b);
+        addToLastFiveCalculationList(String.format("Căn bậc 2 của %.2f : %.2f",a,Math.sqrt(a)));
+        addToLastFiveCalculationList(String.format("Căn bậc 2 của %.2f : %.2f",b,Math.sqrt(b)));
+    }
+}
